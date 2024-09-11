9d663433381fb71e4c693567d93ab91656ef2c8d
ixxeL2097 <fredspiers@gmail.com>
2024-09-11 16:01:59 +0200

    feat: rendered manifest CI2

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+search=".github/workflows/*.yaml"
+
+if [ -f $search ]
+then
+  echo "search is a file"
+elif [ -d $search ]
+then
+  echo "search is a dir"
+fi
+
+for source_file in $search
+do
+  if [ -d $source_file ]
+  then
+    echo "its a dir"
+  else
+    echo "not a dir"
+  fi
+done
+  
